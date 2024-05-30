0be0fa7b99d9f3fbe3190b66c42c3ba6ece6447c
GarryGreh <garik.greh@yandex.ru>
2024-05-30 19:01:26 +0700

    AddSwipe
    Сделал свайп и добавил футбольное поле (текстуру)

--- IgnitFotboll/Assets/_Scripts/PlayerController.cs
+++ IgnitFotboll/Assets/_Scripts/PlayerController.cs
@@ -4,23 +4,74 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed;
-
-    private Vector3 move;
+    //public float speed;
+    [SerializeField]
+    private float speed;
+    private string swipeName;
+    private Vector3 move;    
     private CharacterController characterController;
 
+    private int currentLineMove = 0; // -1 - лева€ полоса, 0 - средн€€, 1 - права€
+    private float distanceToLine = 3;
+
+    private void OnEnable()
+    {
+        Swipe.swipeEvent += SwipeCheck;
+    }
+    private void OnDisable()
+    {
+        Swipe.swipeEvent -= SwipeCheck;
+    }
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
     private void Update()
-    {
-        move = new Vector3(0.0f, 0.0f, 1.0f);
-        characterController.Move(move * speed * Time.deltaTime);
+    {        
+        if(swipeName != null)
+        {
+            if(swipeName == "left")
+            {
+                if(currentLineMove > -1)
+                {
+                    currentLineMove--;
+                    swipeName = null;
+                }
+            }
+            if(swipeName == "right")
+            {
+                if(currentLineMove < 1)
+                {
+                    currentLineMove++;
+                    swipeName = null;
+                }
+            }
+        }
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+        
+        if(currentLineMove == -1)
+        {
+            targetPosition += Vector3.left * distanceToLine;
+        }
+        else if(currentLineMove == 1)
+        {
+            targetPosition += Vector3.right * distanceToLine;
+        }
+        transform.position = targetPosition;
+        //Debug.Log(currentLineMove);
+       // Debug.Log(targetPosition);
+        //move = new Vector3(transform.position.x, transform.position.y, 1.0f);
+        //characterController.Move(move * speed * Time.deltaTime);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -3, 3), 0.0f, transform.position.z);
     }
-    public void SideMove(float _pos)
+    private void FixedUpdate()
     {
-        Debug.Log(_pos);
-        move.x = _pos;
+        move.z = speed;
+        characterController.Move(move * Time.fixedDeltaTime);
+    }
+    public void SwipeCheck(string _swipeName)
+    {
+        //Debug.Log(_swipeName);
+        swipeName = _swipeName;
     }
 }
--- IgnitFotboll/Assets/_Scripts/Swipe.cs
+++ IgnitFotboll/Assets/_Scripts/Swipe.cs
@@ -1,64 +1,140 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Swipe : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Swipe : MonoBehaviour
 {
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        //if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
-        //{
-        //    if (eventData.delta.x > 0)
-        //    {
-        //        Debug.Log(eventData.delta.normalized.x);
-        //    }
-        //    else
-        //    {
-        //        Debug.Log(eventData.delta.normalized.x);
-        //    }
-        //}
-        //else
-        //{
-        //    if(eventData.delta.y > 0)
-        //    {
-        //        Debug.Log(eventData.delta.y);
-        //    }
-        //    else
-        //    {
-        //        Debug.Log(eventData.delta.y);
-        //    }
-        //}
-    }
+    public delegate void OnSwipe(string swipeName);
+    public static event OnSwipe swipeEvent;
 
-    public void OnDrag(PointerEventData eventData)
+    private bool isTap, isLeft, isRight, isUp, isDown;
+
+    private bool isMobile;
+    private bool isSwipe;
+    private Vector2 tapPosition;
+    private Vector2 swipeDelta;
+    public float swipeTrue = 70;
+
+    private void Start()
     {
-        if (Mathf.Abs(eventData.delta.x) > Mathf.Abs(eventData.delta.y))
+        isMobile = Application.isMobilePlatform;
+    }
+    private void Update()
+    {
+         isTap = isLeft = isRight = isUp = isDown = false;
+        
+        if (!isMobile)
         {
-            if (eventData.delta.x > 0)
+            if (Input.GetMouseButtonDown(0))
             {
-                Debug.Log(eventData.delta.normalized.x);
+                isSwipe = true;
+                tapPosition = Input.mousePosition;
+                //isTap = true;
             }
-            else
-            {
-                Debug.Log(eventData.delta.normalized.x);
+            else if (Input.GetMouseButtonUp(0))
+            {              
+                SwipeReset();
             }
         }
         else
         {
-            if (eventData.delta.y > 0)
+            if(Input.touchCount > 0)
             {
-                Debug.Log(eventData.delta.y);
-            }
-            else
-            {
-                Debug.Log(eventData.delta.y);
+                if(Input.GetTouch(0).phase == TouchPhase.Began)
+                {
+                    isSwipe = true;
+                    tapPosition = Input.GetTouch(0).position;
+                   // isTap = true;
+                }
+                else if(Input.GetTouch(0).phase == TouchPhase.Canceled ||
+                    Input.GetTouch(0).phase == TouchPhase.Ended)
+                {
+                    SwipeReset();
+                }
             }
         }
+        SwipeCheck();
     }
-
-    public void OnEndDrag(PointerEventData eventData)
+    private void SwipeReset()
     {
-        
+        isTap = isLeft = isRight = isUp = isDown = false;
+        isSwipe = false;
+
+        tapPosition = Vector2.zero;
+        swipeDelta = Vector2.zero;
+    }
+    private void SwipeCheck()
+    {
+        swipeDelta = Vector2.zero;
+
+        if (isSwipe)
+        {
+            if(!isMobile && Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - tapPosition;
+            }
+            else if(Input.touchCount > 0)
+            {
+                swipeDelta = Input.GetTouch(0).position - tapPosition;
+            }           
+        }
+        if(swipeDelta.magnitude > swipeTrue)
+        {
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if(swipeEvent != null)
+            {
+                if (Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    if(x > 0)
+                    {
+                        isRight = true;                        
+                    }
+                    else
+                    {
+                        isLeft = true;
+                    }
+                }
+                else
+                {
+                    if(y > 0)
+                    {
+                        isUp = true;
+                    }
+                    else
+                    {
+                        isDown = true;
+                    }
+                }
+            }
+            isTap = false;
+            swipeEvent(CheckNameSwipe());
+            SwipeReset();
+        }        
+    }
+    private string CheckNameSwipe()
+    {
+        string nameSwipe = null;
+        if (isTap)
+        {
+            nameSwipe = "tap";
+        }
+        else if (isLeft)
+        {
+            nameSwipe = "left";
+        }
+        else if (isRight)
+        {
+            nameSwipe = "right";
+        }
+        else if (isUp)
+        {
+            nameSwipe = "up";
+        }
+        else if (isDown)
+        {
+            nameSwipe = "down";
+        }
+        return nameSwipe;
     }
 }
